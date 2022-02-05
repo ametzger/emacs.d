e849b8e6e7cc60f70cbeb579031c452e21585fb7
Alex Metzger <asm@asm.io>
2022-02-05 00:57:02 -0500

    lsp: re-enable lsp for rust hackery, need to run `M-x lsp` to activate

--- init.el
+++ init.el
@@ -1410,24 +1410,24 @@ _s-f_: file            _a_: ag                _i_: Ibuffer           _c_: cache 
   (setq markdown-fontify-code-blocks-natively t))
 
 ;; LSP
-;; (use-package lsp-mode
-;;   :ensure
-;;   :commands lsp
-;;   :custom
-;;   ;; what to use when checking on-save. "check" is default, I prefer clippy
-;;   (lsp-rust-analyzer-cargo-watch-command "clippy")
-;;   (lsp-eldoc-render-all t)
-;;   (lsp-idle-delay 0.6)
-;;   :config
-;;   (add-hook 'lsp-mode-hook 'lsp-ui-mode))
+(use-package lsp-mode
+  :ensure
+  :commands lsp
+  :custom
+  ;; what to use when checking on-save. "check" is default, I prefer clippy
+  (lsp-rust-analyzer-cargo-watch-command "clippy")
+  (lsp-eldoc-render-all t)
+  (lsp-idle-delay 0.6)
+  :config
+  (add-hook 'lsp-mode-hook 'lsp-ui-mode))
 
-;; (use-package lsp-ui
-;;   :ensure
-;;   :commands lsp-ui-mode
-;;   :custom
-;;   (lsp-ui-peek-always-show t)
-;;   (lsp-ui-sideline-show-hover t)
-;;   (lsp-ui-doc-enable nil))
+(use-package lsp-ui
+  :ensure
+  :commands lsp-ui-mode
+  :custom
+  (lsp-ui-peek-always-show t)
+  (lsp-ui-sideline-show-hover t)
+  (lsp-ui-doc-enable nil))
 
 ;; python
 (use-package pyenv-mode
