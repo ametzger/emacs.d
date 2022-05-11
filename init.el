684f5c11c572ff14253da92d7b8a7a11a26e655f
Alex Metzger <asm@asm.io>
2022-05-11 17:45:46 -0400

    add elixir-mode

--- init.el
+++ init.el
@@ -1582,6 +1582,34 @@ _s-f_: file            _a_: ag                _i_: Ibuffer           _c_: cache 
               (bind-key "C-a" 'crux-move-beginning-of-line)
               )))
 
+;; elixir
+(use-package elixir-mode
+  :ensure t)
+
+(use-package reformatter
+  :ensure t
+  :config
+  ; Adds a reformatter configuration called "+elixir-format"
+  ; This uses "mix format -"
+  (reformatter-define +elixir-format
+    :program "mix"
+    :args '("format" "-"))
+  ; defines a function that looks for the .formatter.exs file used by mix format
+  (defun +set-default-directory-to-mix-project-root (original-fun &rest args)
+    (if-let* ((mix-project-root (and buffer-file-name
+                                     (locate-dominating-file buffer-file-name
+                                                             ".formatter.exs"))))
+        (let ((default-directory mix-project-root))
+          (apply original-fun args))
+      (apply original-fun args)))
+  ; adds an advice to the generated function +elxir-format-region that sets the proper root dir
+  ; mix format needs to be run from the root directory otherwise it wont use the formatter configuration
+  (advice-add '+elixir-format-region :around #'+set-default-directory-to-mix-project-root)
+  ; Adds a hook to the major-mode that will add the generated function +elixir-format-on-save-mode
+  ; So, every time we save an elixir file it will try to find a .formatter.exs and then run mix format from
+  ; that file's directory
+  (add-hook 'elixir-mode-hook #'+elixir-format-on-save-mode))
+
 ;; golang
 (use-package go-projectile
   :ensure t)
