0221a832de9d731221111046ea6c9222793feb9f
Alex Metzger <asm@asm.io>
2022-07-27 11:08:54 -0400

    dired: add `dwim-shell-command`

--- init.el
+++ init.el
@@ -463,6 +463,24 @@ Repeated invocations toggle between the two most recently open buffers."
     (setq dired-omit-files
           "^\\.?#\\|^.DS_STORE$\\|^.projectile$\\|^.git$\\|^.CFUserTextEncoding$\\|^.Trash$\\|^__pycache__$")))
 
+
+(use-package dwim-shell-command
+  :ensure t
+  :bind (([remap shell-command] . dwim-shell-command)
+         :map dired-mode-map
+         ([remap dired-do-async-shell-command] . dwim-shell-command)
+         ([remap dired-do-shell-command] . dwim-shell-command)
+         ([remap dired-smart-shell-command] . dwim-shell-command))
+  ;; :config
+  ;; (defun my/dwim-shell-command-convert-to-gif ()
+  ;;   "Convert all marked videos to optimized gif(s)."
+  ;;   (interactive)
+  ;;   (dwim-shell-command-on-marked-files
+  ;;    "Convert to gif"
+  ;;    "ffmpeg -loglevel quiet -stats -y -i <<f>> -pix_fmt rgb24 -r 15 <<fne>>.gif"
+  ;;    :utils "ffmpeg"))
+  )
+
 (use-package lisp-mode
   :config
   (add-hook 'emacs-lisp-mode-hook #'eldoc-mode)
