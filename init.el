18ce94b2e6ba62e10be659a12a9ad420acda13fb
Alex Metzger <asm@asm.io>
2023-02-21 10:40:31 -0500

    emacs 29, `tree-sitter` changes

--- init.el
+++ init.el
@@ -593,18 +593,18 @@ Repeated invocations toggle between the two most recently open buffers."
 ;; emacs tools
 (global-set-key (kbd "C-M-.") #'xref-find-definitions-other-window)
 
-(use-package helpful
-  :ensure t
-  :commands (helpful-callable helpful-variable helpful-at-point helpful-command helpful-key)
-  :custom
-  (counsel-describe-function-function #'helpful-callable)
-  (counsel-describe-variable-function #'helpful-variable)
-  :bind
-  ("C-c C-d"                 . helpful-at-point)
-  ([remap describe-function] . counsel-describe-function)
-  ([remap describe-command]  . helpful-command)
-  ([remap describe-variable] . counsel-describe-variable)
-  ([remap describe-key]      . helpful-key))
+;; (use-package helpful
+;;   :ensure t
+;;   :commands (helpful-callable helpful-variable helpful-at-point helpful-command helpful-key)
+;;   :custom
+;;   (counsel-describe-function-function #'helpful-callable)
+;;   (counsel-describe-variable-function #'helpful-variable)
+;;   :bind
+;;   ("C-c C-d"                 . helpful-at-point)
+;;   ([remap describe-function] . counsel-describe-function)
+;;   ([remap describe-command]  . helpful-command)
+;;   ([remap describe-variable] . counsel-describe-variable)
+;;   ([remap describe-key]      . helpful-key))
 
 (use-package s
   :ensure t)
@@ -1243,6 +1243,61 @@ Repeated invocations toggle between the two most recently open buffers."
   :config
   (setq markdown-fontify-code-blocks-natively t))
 
+;; tree-sitter
+; TODO(asm,2023-02-21): make sure this is backwards-compatible until Fedora + homebrew is compatible with 29
+(use-package treesit
+  :preface
+  (defun mp-setup-install-grammars ()
+    "Install Tree-sitter grammars if they are absent."
+    (interactive)
+    (dolist (grammar
+             '((css "https://github.com/tree-sitter/tree-sitter-css")
+               (javascript . ("https://github.com/tree-sitter/tree-sitter-javascript" "master" "src"))
+               (python "https://github.com/tree-sitter/tree-sitter-python")
+               (tsx . ("https://github.com/tree-sitter/tree-sitter-typescript" "master" "tsx/src"))
+               (yaml "https://github.com/ikatyang/tree-sitter-yaml")))
+      (add-to-list 'treesit-language-source-alist grammar)
+      ;; Only install `grammar' if we don't already have it
+      ;; installed. However, if you want to *update* a grammar then
+      ;; this obviously prevents that from happening.
+      (unless (treesit-language-available-p (car grammar))
+        (treesit-install-language-grammar (car grammar)))))
+
+  ;; Optional, but recommended. Tree-sitter enabled major modes are
+  ;; distinct from their ordinary counterparts.
+  ;;
+  ;; You can remap major modes with `major-mode-remap-alist'. Note
+  ;; that this does *not* extend to hooks! Make sure you migrate them
+  ;; also
+  (dolist (mapping '((python-mode . python-ts-mode)
+                     (css-mode . css-ts-mode)
+                     (typescript-mode . tsx-ts-mode)
+                     (js-mode . js-ts-mode)
+                     (css-mode . css-ts-mode)
+                     (yaml-mode . yaml-ts-mode)))
+    (add-to-list 'major-mode-remap-alist mapping))
+
+  :config
+  (mp-setup-install-grammars)
+  ;; Do not forget to customize Combobulate to your liking:
+  ;;
+  ;;  M-x customize-group RET combobulate RET
+  ;;
+  (use-package combobulate
+    ;; Optional, but recommended.
+    ;;
+    ;; You can manually enable Combobulate with `M-x
+    ;; combobulate-mode'.
+    :hook ((python-ts-mode . combobulate-mode)
+           (js-ts-mode . combobulate-mode)
+           (css-ts-mode . combobulate-mode)
+           (yaml-ts-mode . combobulate-mode)
+           (typescript-ts-mode . combobulate-mode)
+           (tsx-ts-mode . combobulate-mode))
+    ;; Amend this to the directory where you keep Combobulate's source
+    ;; code.
+    :load-path ("~/proj/vendor/combobulate"))
+  )
 ;; TODO(asm,2022-10-25): lsp-mode is kind of heavy and more opinionated than I would like, it also
 ;; adds a lot of UI frills that I find unecessary. eglot seems to be more in line with my "I just
 ;; want xref and imenu to pull from LSP" philosophy, but it doesn't seem to integrate with xref
